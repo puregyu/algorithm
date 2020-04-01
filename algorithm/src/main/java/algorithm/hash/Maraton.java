4919556db6cea0d566a7ea269574366c51d75bd7
puregyu <puregyu@naver.com>
2020-04-01 20:42:51 +0900

    init

---
+++ algorithm/src/main/java/algorithm/hash/Maraton.java
@@ -0,0 +1,75 @@
+package algorithm.hash;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Maraton {
+	public static void main(String[] args) {
+		
+		/**
+		 * @문제
+		 	수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+			마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 
+			완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+
+		 */
+		String[] participant = { "abra", "cxmax", "boyond" };
+		String[] completion = { "abra", "cxmax" };
+
+		String answer = "";
+
+		/**
+		 * @푼사람 devyu
+		 * @방법
+		 * ArrayList 이용한 방법
+		 */
+//		ArrayList<String> list = new ArrayList<String>();
+//		for (String p : participant) {
+//			list.add(p);
+//		}
+//		for (String c : completion) {
+//			list.remove(c);
+//		}
+//		answer = list.get(0);
+
+		/**
+		 * @푼사람 해답
+		 * @방법
+		 * HashMap 이용한 방법
+		 */
+//		HashMap<String, Integer> map = new HashMap<String, Integer>();
+//        for (String player : participant) 
+//        	map.put(player, map.getOrDefault(player, 0) + 1);
+//        for (String player : completion) 
+//        	map.put(player, map.get(player) - 1);
+//
+//        for (String key : map.keySet()) {
+//            if (map.get(key) != 0){
+//                answer = key;
+//            }
+//        }		
+       
+		/**
+		 * @푼사람 devyu
+		 * @방법
+		 * Arrays.sort 이용한 방법
+		 */
+//		Arrays.sort(participant);
+//		Arrays.sort(completion);
+//		
+//		int i = 0;
+//		for (i = 0; i < completion.length; i++) {
+//			if (!participant[i].equals(completion[i])) {
+//				break;
+//			}
+//		}
+//		answer = participant[i];
+		
+		
+		// 정답
+		System.out.println(answer);
+
+	}
+}
---
+++ algorithm/src/main/java/algorithm/hash/PhoneNumber.java
@@ -0,0 +1,82 @@
+package algorithm.hash;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class PhoneNumber {
+	
+	public static void main(String[] args) {
+		
+		/**
+		 * @문제
+		 	전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
+			전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다.
+
+			구조대 : 119
+			박준영 : 97 674 223
+			지영석 : 11 9552 4421
+			전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때, 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+
+		 */
+		String[] phone_book = { "119", "97674223", "1195524421" };
+		boolean answer = true;
+		
+		/**
+		 * @푼사람 devyu
+		 * @채점결과
+		 * 정확성: 84.6
+		 * 효율성: 0.0 
+		 * 합계: 84.6 / 100.0 
+		 */
+//		for (int i = 0; i < phone_book.length; i++) {
+//			for (int j = 0; j < phone_book.length; j++) {
+//				if(i != j && phone_book[i].startsWith(phone_book[j])) {
+//					answer = false;
+//				}
+//			}
+//		}
+		
+		/**
+		 * @푼사람 devyu
+		 * @채점결과
+		 * 채점 결과
+		 * 정확성: 69.2
+		 * 효율성: 15.4
+		 * 합계: 84.6 / 100.0
+		 */
+//		for (int i = 0; i < phone_book.length-1; i++) {
+//			for (int j = i+1; j < phone_book.length; j++) {
+//				if(phone_book[j].startsWith(phone_book[i])) {
+//					answer = false;
+//					break;
+//				}
+//			}
+//		}
+		
+
+		/**
+		 * @푼사람 devyu
+		 * @채점결과
+		 * 채점 결과
+		 * 합격
+		 * 
+		 * Collections.sort() 를 사용한 
+		 */
+		List<String> sortedList = new ArrayList<>(Arrays.asList(phone_book));
+		Collections.sort(sortedList);
+		
+		for (int i = 0; i < sortedList.size()-1; i++) {
+			for (int j = i+1; j < sortedList.size(); j++) {
+				if(sortedList.get(j).startsWith(sortedList.get(i))) {
+					answer = false;
+					break;
+				}
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}
